7ea1132502380540375c50a89018d882b123aa61
Steff Beckers <steff@steffbeckers.eu>
2019-10-16 18:30:49 +0200

    League messaging SAVE

---
+++ back-end/SC.API/GraphQL/Messaging/LeagueUpdatedMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SC.API.GraphQL.Messaging
+{
+    public class LeagueUpdatedMessage
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Season { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
