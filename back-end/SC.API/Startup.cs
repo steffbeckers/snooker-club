6fa7d34af97bb06acfd65aacb9481494a193abed
Steff Beckers <beckerst@aarixa.be>
2019-10-16 16:51:20 +0200

    app.UseAuthorization(); missed

--- back-end/SC.API/Startup.cs
+++ back-end/SC.API/Startup.cs
@@ -69,11 +69,11 @@ namespace SC.API
 
             // Authentication and Authorization
             services.AddAuthentication();
+            services.AddAuthorization();
             services.AddIdentity<User, IdentityRole<Guid>>()
                 .AddRoleManager<RoleManager<IdentityRole<Guid>>>()
                 .AddEntityFrameworkStores<SCContext>()
                 .AddDefaultTokenProviders();
-            services.AddAuthorization();
 
             services.Configure<IdentityOptions>(options =>
             {
@@ -187,8 +187,9 @@ namespace SC.API
             app.UseGraphQL<SCSchema>();
             app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
 
-            // Authentication
+            // Authentication and Authorization
             app.UseAuthentication();
+            app.UseAuthorization();
             CreateDefaultRolesAndAdminUser(serviceProvider);
 
             // CORS
